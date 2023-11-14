cc707e969c9846fb8b0ad67337144e3dd33a96f7
fsavio-lab <fsavio27@gmail.com>
2023-11-14 16:39:14 +0530

    Implemented Responsive Scaling Constants

--- src/components/Header/DefaultHeader.tsx
+++ src/components/Header/DefaultHeader.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { DrawerHeaderProps } from '@react-navigation/drawer'
+import { Fonts } from '../../constants/Fonts'
+import { moderateScale } from '../../constants/Metrics'
 
 export interface DefaultHeaderProps extends DrawerHeaderProps{
 
@@ -12,12 +14,16 @@ const DefaultHeader = (props: DefaultHeaderProps) => {
     headerContainer:{
       height: 64,
       backgroundColor: 'red'
+    },
+    headerText:{
+      fontSize: moderateScale(16),
+      fontFamily: Fonts.SourceSans3.Black
     }
   })
 
   return (
     <View style={styles.headerContainer}>
-      <Text>DefaultHeader</Text>
+      <Text style={styles.headerText}>DefaultHeader</Text>
     </View>
   )
 }
