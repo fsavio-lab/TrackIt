40c5ae35d9331b7dfd9fc822051726fe3d3d8f78
fsavio-lab <fsavio27@gmail.com>
2023-11-14 14:41:19 +0530

    Fonts and Static Images Setup

---
+++ src/constants/Fonts.ts
@@ -0,0 +1,34 @@
+export const Fonts ={
+    SourceSans3 :{
+        ExtraLight: "SourceSans3-ExtraLight",
+        ExtraLightItalic: "SourceSans3-ExtraLightItalic",
+        Light: "SourceSans3-Light",
+        LightItalic: "SourceSans3-LightItalic",
+        Regular: "SourceSans3-Regular",
+        RegularItalic: "SourceSans3-Italic",
+        Medium: "SourceSans3-Medium",
+        MediumItalic: "SourceSans3-MediumItalic",
+        SemiBold: "SourceSans3-SemiBold",
+        SemiBoldItalic: "SourceSans3-SemiBoldItalic",
+        Bold: "SourceSans3-Bold",
+        BoldItalic: "SourceSans3-BoldItalic",
+        Black:"SourceSans3-Black",
+        BlackItalic: "SourceSans3-BlackItalic"
+    },
+    Lato :{
+        ExtraLight: "Lato-ExtraLight",
+        ExtraLightItalic: "Lato-ExtraLightItalic",
+        Light: "Lato-Light",
+        LightItalic: "Lato-LightItalic",
+        Regular: "Lato-Regular",
+        RegularItalic: "Lato-Italic",
+        Medium: "Lato-Medium",
+        MediumItalic: "Lato-MediumItalic",
+        SemiBold: "Lato-SemiBold",
+        SemiBoldItalic: "Lato-SemiBoldItalic",
+        Bold: "Lato-Bold",
+        BoldItalic: "Lato-BoldItalic",
+        Black:"Lato-Black",
+        BlackItalic: "Lato-BlackItalic"
+    }
+}
